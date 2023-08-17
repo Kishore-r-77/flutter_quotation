0a88e07a860186bf2d20973aeed566b83f75fb8e
Kishore-r-77 <kishorework77@gmail.com>
2023-08-17 16:11:14 +0530

    added card in every attribute

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -14,36 +14,56 @@ class Profile extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.all(8.0),
         children: <Widget>[
-          Text(
-            "Name: ${loginResponse["name"]}",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Theme.of(context).colorScheme.primary,
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Name: ${loginResponse["name"]}",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
             ),
           ),
-          Text(
-            "Email: ${loginResponse["email"]}",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Theme.of(context).colorScheme.primary,
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Email: ${loginResponse["email"]}",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
             ),
           ),
-          Text(
-            "phone: ${loginResponse["phone"]}",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Theme.of(context).colorScheme.primary,
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "phone: ${loginResponse["phone"]}",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
             ),
           ),
-          Text(
-            "Company Name: ${loginResponse["companyName"]}",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Theme.of(context).colorScheme.primary,
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Company Name: ${loginResponse["companyName"]}",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
             ),
           ),
         ],
